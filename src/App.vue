69a9ff9ec2429d5da65b2c33c95b328585213f0c
Ethys <ethbergmuzola500@gmail.com>
2025-11-28 12:54:30 +0100

    feat: add App.vue component

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="min-h-screen bg-white">
+    <Header />
+    <Hero />
+    <Technologies />
+    <About />
+    <Skills />
+    <Projects />
+    <Resume />
+    <Services />
+    <Testimonials />
+    <Contact />
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from './components/Header.vue';
+import Hero from './components/Hero.vue';
+import Technologies from './components/Technologies.vue';
+import About from './components/About.vue';
+import Skills from './components/Skills.vue';
+import Projects from './components/Projects.vue';
+import Resume from './components/Resume.vue';
+import Services from './components/Services.vue';
+import Testimonials from './components/Testimonials.vue';
+import Contact from './components/Contact.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+</style>
