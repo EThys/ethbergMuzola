8a0515cd32db802fb3b81e9237e2b528853ef5a7
Ethys <ethbergmuzola500@gmail.com>
2025-11-29 13:42:26 +0100

    feat: Intégration router et loader dans App

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,17 @@
 <template>
   <div id="app" class="min-h-screen bg-white">
-    <Loader />
-    <Header />
-    <Hero />
-    <About />
-    <Skills />
-    <Projects />
-    <Resume />
-    <Services />
-    <Testimonials />
-    <Contact />
-    <Footer />
-    <ProjectModal />
+    <Loader v-if="showLoader" />
+    <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
+import { RouterView, useRoute } from 'vue-router';
+import { computed } from 'vue';
 import Loader from './components/Loader.vue';
-import Header from './components/Header.vue';
-import Hero from './components/Hero.vue';
-import About from './components/About.vue';
-import Skills from './components/Skills.vue';
-import Projects from './components/Projects.vue';
-import Resume from './components/Resume.vue';
-import Services from './components/Services.vue';
-import Testimonials from './components/Testimonials.vue';
-import Contact from './components/Contact.vue';
-import Footer from './components/Footer.vue';
-import ProjectModal from './components/ProjectModal.vue';
+
+const route = useRoute();
+const showLoader = computed(() => route.path === '/');
 </script>
 
 <style>
